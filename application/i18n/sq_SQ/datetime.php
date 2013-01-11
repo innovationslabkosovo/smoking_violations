3963f0b05f3f36d5c52fe8259131d2e4708cee27
innovationslabkosovo <designcenter@kosovoinnovations.org>
2013-01-11 18:29:37 +0100

    Adding second release

---
+++ application/i18n/sq_SQ/datetime.php
@@ -0,0 +1,81 @@
+<?php
+	$lang = array(
+	'am' => 'paradite',
+	'friday' => array(
+		'abbv' => 'Premt',
+		'full' => ' E Premte',
+	),
+	'monday' => array(
+		'abbv' => 'H&euml;n',
+		'full' => ' E H&euml;n&euml;',
+	),
+	'pm' => 'pasdite',
+	'saturday' => array(
+		'abbv' => 'Shtun',
+		'full' => ' E Shtun&euml;',
+	),
+	'sunday' => array(
+		'abbv' => 'Diel',
+		'full' => ' E Diel',
+	),
+	'thursday' => array(
+		'abbv' => 'Enj',
+		'full' => ' E Enjte',
+	),
+	'tuesday' => array(
+		'abbv' => 'Mar',
+		'full' => ' E Mart&euml;',
+	),
+	'wednesday' => array(
+		'abbv' => 'M&euml;r',
+		'full' => ' E M&euml;rkure',
+	),
+	'January' => array(
+		'abbv' => 'Jan',
+		'full' => 'Janar',
+	),
+	'February' => array(
+		'abbv' => 'Shk',
+		'full' => 'Shkurt',
+	),
+	'March' => array(
+		'abbv' => 'Mar',
+		'full' => 'Mars',
+	),
+	'April' => array(
+		'abbv' => 'Pri',
+		'full' => 'Prill',
+	),
+	'May' => array(
+		'abbv' => 'Maj',
+		'full' => 'Maj',
+	),
+	'June' => array(
+		'abbv' => 'Qer',
+		'full' => 'Qershor',
+	),
+	'July' => array(
+		'abbv' => 'Kor',
+		'full' => 'Korrik',
+	),
+	'August' => array(
+		'abbv' => 'Gsh',
+		'full' => 'Gusht',
+	),
+	'September' => array(
+		'abbv' => 'Sht',
+		'full' => 'Shtator',
+	),
+	'October' => array(
+		'abbv' => 'Tet',
+		'full' => 'Tetor',
+	),
+	'November' => array(
+		'abbv' => 'N&euml;n',
+		'full' => 'N&euml;ntor',
+	),
+	'December' => array(
+		'abbv' => 'Dhj',
+		'full' => 'Dhjetor',
+	));
+?>
